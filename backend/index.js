0927e256aba06c90fd97b942da5ed3460202f0ba
riswanfauzan25 <riswanfauzan25@gmail.com>
2026-07-31 23:47:03 +0700

    feat: initialize Express server with modular API route configuration

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,15 @@ const medicalRecordRoutes = require('./routes/medicalRecordRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 
-app.use(cors());
+// Menangani preflight request secara manual agar aman di Vercel
+app.options('*', cors());
+
 app.use(express.json());
 
 // Routes API Clean Modular Mapping
